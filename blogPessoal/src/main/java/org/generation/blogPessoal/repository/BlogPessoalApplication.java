1b4b508dcd94482afd5f4be4e88894d733591d08
unknown <viniciuslavsilva@gmail.com>
2021-07-01 21:07:54 -0300

    Springboot

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/BlogPessoalApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class BlogPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoalApplication.class, args);
+	}	
+}
