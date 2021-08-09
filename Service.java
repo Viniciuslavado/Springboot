2d24b57190a1b049a2909038edd93e9955896cf2
Vinicius Lavado <85173166+Viniciuslavado@users.noreply.github.com>
2021-08-09 20:15:33 -0300

    Add files via upload

---
+++ Service.java
@@ -0,0 +1,27 @@
+package org.generation.blogPessoal.security;
+
+import java.util.Optional;
+
+import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.usuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@org.springframework.stereotype.Service
+
+public class Service implements UserDetailsService {
+
+	@Autowired
+	private usuarioRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException{
+		Optional<Usuario> user = userRepository.findByUsuario(userName);
+		user.orElseThrow(() -> new UsernameNotFoundException(userName + "not found."));
+		return user.map(userDetailsImplementacao::new).get();
+	}
+
+
+}
